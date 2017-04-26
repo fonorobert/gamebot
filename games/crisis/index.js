63994c800b296a03065b48370955ef3c0b00269f
Jaan Lambert <jaan.lambert@gmail.com>
2017-04-26 16:27:48 +0200

    Players and cards

--- games/crisis/index.js
+++ games/crisis/index.js
@@ -1,4 +1,6 @@
+import _ from 'lodash'
 import fs from 'fs'
+import Player from './player.js'
 
 export default function (config, { sendMessage, onFinish }, { maxIterations = 3 }) {
   const { bot, channel, users, savedState } = config
@@ -15,9 +17,43 @@ export default function (config, { sendMessage, onFinish }, { maxIterations = 3 
         this.load().then((cards) => {
           sendMessage(config, 'EXISTENTIAL CRISIS')
           sendMessage(config, `Loaded ${cards.length} cards`)
-          this.cards = cards
+          this.cards = _.shuffle(cards)
+          this.initPlayers()
         })
       }
+    }
+
+    initPlayers () {
+      state.players = users.map((userData) => {
+        const cards = [this.takeCard(), this.takeCard()]
+        return new Player({
+          ...userData,
+          cards
+        })
+      })
+      state.currentPlayer = Math.floor(Math.random() * state.players.length)
+
+      const printCards = (cards) => {
+        return cards.reduce((result, card, index) => {
+          return result += `${index + 1}. ${card.description} \n`
+        }, '')
+      }
+
+      state.players.forEach((player) => {
+        sendMessage(config, `${player.name}, your cards are: \n${printCards(player.cards)}`)
+      })
+    }
+
+    takeCard () {
+      return this.cards.pop()
+    }
+
+    getCurrentPlayer () {
+      return state.players[state.currentPlayer]
+    }
+
+    nextPlayer () {
+
     }
 
     stop () {
@@ -25,7 +61,16 @@ export default function (config, { sendMessage, onFinish }, { maxIterations = 3 
     }
 
     processMessage (message) {
+      const PLAY_REGEX = /^play\s(\d.*)$/
+      const currentPlayer = this.getCurrentPlayer()
+      if (message.user === currentPlayer.id) {
 
+        const playMatch = message.text.match(PLAY_REGEX)
+        if (playMatch) {
+          const cardIndex = +playMatch[1]
+          console.log(cardIndex)
+        }
+      }
     }
 
     load () {
---
+++ games/crisis/player.js
@@ -0,0 +1,17 @@
+class Player {
+  constructor ({ id, name, cards }) {
+    this.id = id
+    this.name = name
+    this.cards = cards || []
+  }
+
+  playCard (index) {
+
+  }
+
+  addCard () {
+
+  }
+}
+
+export default Player
