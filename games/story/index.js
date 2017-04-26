5861ee7d00a79403c31903c7223c3895be58cc5a
Henrique Pinheiro <github@gampoc.com>
2017-04-26 15:14:56 +0200

    Handle titles; Post story to channel.

--- games/story/index.js
+++ games/story/index.js
@@ -2,6 +2,7 @@ import _ from 'lodash'
 import colors from '../../utils/colors'
 
 const TIME_LIMIT = 40000
+const STORY_CHANNEL = "ex-machina"
 
 export default function (config, { sendMessage, onFinish }, { maxIterations=3 }) {
   const { bot, channel, users, savedState } = config
@@ -41,9 +42,12 @@ export default function (config, { sendMessage, onFinish }, { maxIterations=3 })
     },
 
     processMessage: function (message) {
+      clearTimeout(state.timeout)
       if (message.text === 'THE END') {
-        return this.finish()
+        return this.askForTitle()
       }
+
+      if (state.processingTitles) return this.processTitle(message)
 
       const words = message.text.split(/\s+/)
       if (words.length !== 3) return
@@ -54,7 +58,6 @@ export default function (config, { sendMessage, onFinish }, { maxIterations=3 })
         return
       }
 
-      clearTimeout(state.timeout)
       state.playCount += 1
       if (state.playCount % state.userCount === 0) state.iterationCount += 1
       state.story += message.text.trim() + ' '
@@ -62,7 +65,7 @@ export default function (config, { sendMessage, onFinish }, { maxIterations=3 })
       this.nextUser()
 
       if (state.iterationCount == state.maxIterations) {
-        return this.finish()
+        return this.askForTitle()
       } else {
         sendMessage(config, `Cool. Next is *${this.getCurrentUserName()}*!`)
       }
@@ -75,20 +78,52 @@ export default function (config, { sendMessage, onFinish }, { maxIterations=3 })
     serialize: function () {
     },
 
+    askForTitle: function() {
+      state.processingTitles = true
+      state.titles = {}
+      sendMessage(config, `Nice one! Now all I need is the title. Anyone?`)
+      state.titleTimeout = setTimeout(this.handleTitleTimeout.bind(this), TIME_LIMIT)
+    },
+
+    processTitle: function(message) {
+      if (state.titles[message.user]) return sendMessage(config, 'I mean... Anyone ELSE?')
+      state.titles[message.user] = message.text.trim()
+      clearTimeout(state.titleTimeout)
+      if (Object.keys(state.titles).length >= users.length) {
+        sendMessage(config, `Great! I'll just choose one of these. I'll post this story to #${STORY_CHANNEL}.`)
+        return this.finish()
+      }
+      state.titleTimeout = setTimeout(this.handleTitleTimeout.bind(this), TIME_LIMIT)
+      return sendMessage(config, 'Good. Anyone else?')
+    },
+
+    handleTitleTimeout: function() {
+      if (Object.keys(state.titles).length === 0) {
+        sendMessage(config, 'Still need that title, guys!')
+        state.titleTimeout = setTimeout(this.handleTitleTimeout.bind(this), TIME_LIMIT)
+        return
+      }
+      sendMessage(config, `I think that\'s enough titles, then. I'll post this story to <#${STORY_CHANNEL}|${STORY_CHANNEL}>.`)
+      this.finish()
+    },
+
     finish: function () {
       let authors = _.map(users, (user) => '@' + user.name).join(', ')
       const lastComma = authors.lastIndexOf(', ')
       authors = authors.substring(0, lastComma) + ' and ' + authors.substring(lastComma + 2)
 
-      const pretext = 'Here\'s the complete story:'
-      const text = `${state.story}\n\nby ${authors}`
+      const title = _.shuffle(state.titles)[0]
+      const pretext = `*${title}*\n_a story by ${authors}_`
+      const text = `${state.story}`
       const fallback = `${pretext}\n${text}`
-      sendMessage(config, {
+      sendMessage(_.assign({}, config, {channel: STORY_CHANNEL}), {
         attachments: [
           {
+            fallback,
             pretext,
             text,
-            color: colors.success
+            color: colors.success,
+            mrkdwn_in: ["pretext", "text"],
           }
         ]
       })
